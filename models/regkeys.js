1d8a36d104f85f5d0fd425f871e1a4fea949c1bc
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2018-01-07 04:06:59 +0530

    Initial setup for making npm bundle

--- models/regkeys.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var Regkeys = new Schema({
-	regkey : { type: String, required: true},
-	isUsed : { type: Boolean, required: true},
-});
-
-module.exports = mongoose.model('Regkeys', Regkeys);
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var Account = require('../models/account');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	if(req.user){
-		res.redirect('/cargo/');
-	}
-	else{
-		res.render('index', { user : req.user});
-	}
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
