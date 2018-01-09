9850f09a42ddbc8c6c0a9f532c735634a5efa64f
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2018-01-09 19:02:04 +0530

    fs files integration

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	fs.readdir(process.cwd(), (err, files) => {
+	  	files.forEach(file => {
+	    	console.log(file);
+	  	});
+	  	res.render('index', { filesList: files });
+	})
 });
 
 module.exports = router;
